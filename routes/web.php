c139c3dd1118fa1d95b23ce26930b9d13e09caad
sakaokimiaki <sakaokimiaki33@yahoo.co.jp>
2020-11-26 12:42:59 +0900

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $books = Book::all(); 
+    return view('books', ['books' => $books ]);
+})->middleware('auth');
+
+Route::post('/book', function(Request $request){
+    $validator = Validator::make($request->all(),[
+        'name' => 'required|max:255',
+    ]);
+
+    $book = new Book;
+    $book->title = $request->name;
+    $book->save();
+
+    return redirect('/');
+});
+
+Route::delete('/book/{book}', function(Book $book){
+    $book->delete();
+
+    return redirect('/');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
